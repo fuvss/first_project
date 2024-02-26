7554dee995334187f111786f24d49b3ea4f345c9
farwa <farwali2003@gmail.com>
2024-02-26 12:58:10 +0500

    new branch made

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ import mongoose from "mongoose";
 import router from "./api/user.routes.js";
 import UserModel from "./api/user.model.js"; // Import User model
 
-
 dotenv.config();
 
-const App = express();
-App.use(express.json());
+const startServer = async () => {
+  const App = express();
+  App.use(express.json());
 
-await mongoose
-  .connect(process.env.MONGODBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("connected to mongoDB"))
-  .catch((err) => console.log("Error connecting to MongoDB", err));
+  try {
+    await mongoose.connect(process.env.MONGODBURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
 
-App.get("/", (req, res) => {
-  res.send("HELLOO");
-});
+    App.get("/", (req, res) => {
+      res.send("HELLOO");
+    });
 
-App.use("/users", router);
+    App.use("/users", router);
 
-const port = 5000;
+    const port = process.env.PORT || 5000;
+    App.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+  }
+};
 
-App.listen(port, () => {
-  console.log(`server is running on port, ${port}`);
-});
+startServer();
